74acc362bf8db484be8901c6f5fde7e62d15fe2a
Danielle Bunker <daniellebunker8@gmail.com>
2021-04-08 02:20:12 +0000

    Added conditionals to AbsractMelonOrder get_total

--- melons.py
+++ melons.py
@@ -13,10 +13,16 @@ class AbstractMelonOrder():
 
     def get_total(self):
         """Calculate price, including tax."""
-
         base_price = 5
-        total = (1 + self.tax) * self.qty * base_price
 
+        if self.species == "Christmas":
+            base_price = base_price * 1.5
+        
+        total = (1 + self.tax) * self.qty * base_price
+        
+        if self.order_type == "international" and self.qty > 10:
+            total = total + 3
+        
         return total
 
     def mark_shipped(self):
